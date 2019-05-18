2a0667937606442726274544b0ab8d93b8b6dd35
dominisz <dominisz@gmail.com>
2019-05-18 10:33:03 +0200

    Add missing classes

--- src/main/java/pl/dominisz/springintroduction/service/PaypalCreditCardProcessor.java
+++ src/main/java/pl/dominisz/springintroduction/service/PaypalCreditCardProcessor.java
@@ -1,8 +1,13 @@
 package pl.dominisz.springintroduction.service;
 
+import java.math.BigDecimal;
+
 /**
  * http://dominisz.pl
  * 18.05.2019
  */
 public class PaypalCreditCardProcessor implements CreditCardProcessor {
+    public ChargeResult charge(CreditCard creditCard, BigDecimal amount) {
+        return null;
+    }
 }
