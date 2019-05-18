20c8791b674a5244317f71ad134f452ab53e35ee
dominisz <dominisz@gmail.com>
2019-05-18 12:47:35 +0200

    Add some order items

--- src/main/java/pl/dominisz/springintroduction/Application.java
+++ src/main/java/pl/dominisz/springintroduction/Application.java
@@ -2,18 +2,31 @@ package pl.dominisz.springintroduction;
 
 import pl.dominisz.springintroduction.model.CreditCard;
 import pl.dominisz.springintroduction.model.Order;
+import pl.dominisz.springintroduction.model.OrderItem;
 import pl.dominisz.springintroduction.model.Receipt;
 import pl.dominisz.springintroduction.service.*;
 
+import java.math.BigDecimal;
 import java.time.LocalDate;
 
 public class Application {
 
     public static void main(String[] args) {
 
+        double suma = 0.0;
+        for (int i = 0; i < 10; i++) {
+            suma += 0.1;
+        }
+        System.out.println(suma);
+
         BillingService billingService = new CreditCardBillingService();
 
         Order order = new Order();
+        OrderItem hotDog = new OrderItem("Hot dog", new BigDecimal("3.59"));
+        OrderItem coffee = new OrderItem("Coffee", new BigDecimal("4.99"));
+        order.addItem(hotDog);
+        order.addItem(coffee);
+
         CreditCard creditCard = new CreditCard("Imie", "Nazwisko", "123", LocalDate.of(2022, 5, 1));
 
         Receipt receipt = billingService.chargeOrder(order, creditCard);
