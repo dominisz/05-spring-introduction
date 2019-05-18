8d375d3b71083fc27c78568d5d05c614dd8def02
dominisz <dominisz@gmail.com>
2019-05-18 14:13:21 +0200

    More tests of CreditCardBillingService

--- src/test/java/pl/dominisz/springintroduction/service/CreditCardBillingServiceTest.java
+++ src/test/java/pl/dominisz/springintroduction/service/CreditCardBillingServiceTest.java
@@ -3,6 +3,7 @@ package pl.dominisz.springintroduction.service;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
+import pl.dominisz.springintroduction.exception.UnreachableException;
 import pl.dominisz.springintroduction.model.*;
 
 import java.math.BigDecimal;
@@ -62,6 +63,24 @@ public class CreditCardBillingServiceTest {
 
     @Test
     void shouldFailCharge() {
-        //TODO zaimplementować
+        Order order = new Order();
+        OrderItem hotDog = new OrderItem("Hot dog", new BigDecimal("3.59"));
+        OrderItem coffee = new OrderItem("Coffee", new BigDecimal("4.99"));
+        order.addItem(hotDog);
+        order.addItem(coffee);
+
+        CreditCard creditCard = new CreditCard("Imie", "Nazwisko", "123", LocalDate.of(2022, 5, 1));
+
+        CreditCardProcessor creditCardProcessor = Mockito.mock(CreditCardProcessor.class);
+        Mockito.when(creditCardProcessor.charge(creditCard, order.getAmount()))
+                .thenThrow(new UnreachableException("broken network"));
+
+        TransactionLog transactionLog = Mockito.mock(TransactionLog.class);
+        CreditCardBillingService creditCardBillingService = new CreditCardBillingService(creditCardProcessor, transactionLog);
+
+        Receipt receipt = creditCardBillingService.chargeOrder(order, creditCard);
+
+        Assertions.assertFalse(receipt.isSuccessful());
+        Assertions.assertEquals("broken network", receipt.getMessage());
     }
 }
