a71d20fec5580a2fb306ab18c0c5cb0dd905c149
dominisz <dominisz@gmail.com>
2019-05-18 14:10:09 +0200

    More tests of CreditCardBillingService

--- src/test/java/pl/dominisz/springintroduction/service/CreditCardBillingServiceTest.java
+++ src/test/java/pl/dominisz/springintroduction/service/CreditCardBillingServiceTest.java
@@ -39,7 +39,25 @@ public class CreditCardBillingServiceTest {
 
     @Test
     void shouldDeclineCharge() {
-        //TODO zaimplementować
+        Order order = new Order();
+        OrderItem hotDog = new OrderItem("Hot dog", new BigDecimal("3.59"));
+        OrderItem coffee = new OrderItem("Coffee", new BigDecimal("4.99"));
+        order.addItem(hotDog);
+        order.addItem(coffee);
+
+        CreditCard creditCard = new CreditCard("Imie", "Nazwisko", "123", LocalDate.of(2022, 5, 1));
+
+        CreditCardProcessor creditCardProcessor = Mockito.mock(CreditCardProcessor.class);
+        Mockito.when(creditCardProcessor.charge(creditCard, order.getAmount()))
+                .thenReturn(new ChargeResult(false, "error"));
+
+        TransactionLog transactionLog = Mockito.mock(TransactionLog.class);
+        CreditCardBillingService creditCardBillingService = new CreditCardBillingService(creditCardProcessor, transactionLog);
+
+        Receipt receipt = creditCardBillingService.chargeOrder(order, creditCard);
+
+        Assertions.assertFalse(receipt.isSuccessful());
+        Assertions.assertEquals("error", receipt.getMessage());
     }
 
     @Test
