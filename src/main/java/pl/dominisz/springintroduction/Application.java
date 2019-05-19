7d7ceb7811750f3dd3d069f457a7fd1e7d2704fc
dominisz <dominisz@gmail.com>
2019-05-19 13:39:33 +0200

    Add SpringBoot starters

--- src/main/java/pl/dominisz/springintroduction/Application.java
+++ src/main/java/pl/dominisz/springintroduction/Application.java
@@ -1,35 +1,13 @@
 package pl.dominisz.springintroduction;
 
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.context.annotation.ComponentScan;
-import pl.dominisz.springintroduction.model.CreditCard;
-import pl.dominisz.springintroduction.model.Order;
-import pl.dominisz.springintroduction.model.OrderItem;
-import pl.dominisz.springintroduction.model.Receipt;
-import pl.dominisz.springintroduction.service.*;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.math.BigDecimal;
-import java.time.LocalDate;
-
-@ComponentScan
+@SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
-        Order order = new Order();
-        OrderItem hotDog = new OrderItem("Hot dog", new BigDecimal("3.59"));
-        OrderItem coffee = new OrderItem("Coffee", new BigDecimal("4.99"));
-        order.addItem(hotDog);
-        order.addItem(coffee);
-
-        CreditCard creditCard = new CreditCard("Imie", "Nazwisko", "123", LocalDate.of(2022, 5, 1));
-
-        ApplicationContext context = new AnnotationConfigApplicationContext(Application.class);
-
-        BillingService billingService = context.getBean(BillingService.class);
-        Receipt receipt = billingService.chargeOrder(order, creditCard);
-
-        System.out.println(receipt);
+        SpringApplication.run(Application.class, args);
     }
 
 }
