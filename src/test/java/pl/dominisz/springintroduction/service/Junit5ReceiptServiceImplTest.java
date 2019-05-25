4299b1de821b64508f9fa61c82877ae40890e0ae
dominisz <dominisz@gmail.com>
2019-05-25 13:34:04 +0200

    ReceiptServiceImpl unit tests

--- src/test/java/pl/dominisz/springintroduction/service/Junit5ReceiptServiceImplTest.java
+++ src/test/java/pl/dominisz/springintroduction/service/Junit5ReceiptServiceImplTest.java
@@ -1,6 +1,5 @@
 package pl.dominisz.springintroduction.service;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
@@ -9,11 +8,13 @@ import pl.dominisz.springintroduction.exception.OrderNotFoundException;
 import pl.dominisz.springintroduction.exception.UserNotFoundException;
 import pl.dominisz.springintroduction.model.CreditCard;
 import pl.dominisz.springintroduction.model.Order;
+import pl.dominisz.springintroduction.model.Receipt;
 import pl.dominisz.springintroduction.model.User;
 import pl.dominisz.springintroduction.repository.OrderRepository;
 import pl.dominisz.springintroduction.repository.ReceiptRepository;
 import pl.dominisz.springintroduction.repository.UserRepository;
 
+import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.Optional;
 
@@ -31,6 +32,9 @@ public class Junit5ReceiptServiceImplTest {
     private static final String LAST_NAME = "lastName";
     private static final String NUMBER = "123567890";
     private static final LocalDate EXPIRY_DATE = LocalDate.of(2020, 1, 1);
+    private static final BigDecimal AMOUNT = BigDecimal.TEN;
+    private static final String MESSAGE = "message";
+    private static final boolean SUCCESSFUL = true;
 
     private final long USER_ID = 1;
     private final long ORDER_ID = 1;
@@ -88,6 +92,24 @@ public class Junit5ReceiptServiceImplTest {
 
     @Test
     public void shouldCreateReceipt() {
+        User user = createUser();
+        Order order = createOrder();
+        Receipt receipt = new Receipt(AMOUNT, MESSAGE, SUCCESSFUL);
 
+        when(userRepository.findById(USER_ID)).thenReturn(Optional.of(user));
+        when(orderRepository.findById(ORDER_ID)).thenReturn(Optional.of(order));
+        when(billingService.chargeOrder(order, user.getCreditCard())).thenReturn(receipt);
+        when(receiptRepository.save(receipt)).thenReturn(receipt);
+
+        Receipt actualReceipt = receiptService.create(createCreateReceiptDto());
+
+        assertEquals(receipt, actualReceipt);
     }
+
+    private Order createOrder() {
+        Order order = new Order();
+        order.setId(ORDER_ID);
+        return order;
+    }
+
 }
