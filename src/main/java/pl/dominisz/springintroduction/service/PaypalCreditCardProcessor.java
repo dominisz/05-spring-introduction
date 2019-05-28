b536dda668fe2e995b6f423147f79e8c23bd0ecd
dominisz <dominisz@gmail.com>
2019-05-28 22:06:32 +0200

    Use RestTemplate for connecting to CreditCardApplication

--- src/main/java/pl/dominisz/springintroduction/service/PaypalCreditCardProcessor.java
+++ src/main/java/pl/dominisz/springintroduction/service/PaypalCreditCardProcessor.java
@@ -1,12 +1,18 @@
 package pl.dominisz.springintroduction.service;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestClientException;
+import org.springframework.web.client.RestTemplate;
+import pl.dominisz.creditcardmodel.CreditCardOperation;
+import pl.dominisz.creditcardmodel.CreditCardOperationResult;
+import pl.dominisz.creditcardmodel.CreditCardOperationStatus;
+import pl.dominisz.creditcardmodel.CreditCardOperationType;
 import pl.dominisz.springintroduction.exception.UnreachableException;
 import pl.dominisz.springintroduction.model.ChargeResult;
 import pl.dominisz.springintroduction.model.CreditCard;
 
 import java.math.BigDecimal;
-import java.util.Random;
 
 /**
  * http://dominisz.pl
@@ -15,19 +21,33 @@ import java.util.Random;
 @Component
 public class PaypalCreditCardProcessor implements CreditCardProcessor {
 
-    private Random random = new Random();
+    @Value("${creditcardapplication.server}")
+    private String creditCardServerUrl;
 
     public ChargeResult charge(CreditCard creditCard, BigDecimal amount) {
-        int number = random.nextInt(3);
-        if (number == 0) {
-            System.out.println("I'm charging card " + creditCard.getNumber() + " for " + amount);
-            return new ChargeResult(true, "Successful charge");
-        } else if (number == 1) {
-            System.out.println("Charging card " + creditCard.getNumber() + " failed");
-            return new ChargeResult(false, "Limits exceeded");
-        } else {
-            System.out.println("Broken network connection");
-            throw new UnreachableException("Broken network connection");
+        RestTemplate restTemplate = new RestTemplate();
+
+        CreditCardOperation operation = createChargeOperation(creditCard, amount);
+
+        try {
+            CreditCardOperationResult operationResult =
+                    restTemplate.postForObject(creditCardServerUrl, operation, CreditCardOperationResult.class);
+
+            if (operationResult != null) {
+                return new ChargeResult(operationResult.getStatus().equals(CreditCardOperationStatus.OK), operationResult.getMessage());
+            } else {
+                return new ChargeResult(false, "Null response");
+            }
+        } catch (RestClientException exception) {
+            throw new UnreachableException(exception.getMessage());
         }
+    }
+
+    private CreditCardOperation createChargeOperation(CreditCard creditCard, BigDecimal amount) {
+        CreditCardOperation operation = new CreditCardOperation();
+        operation.setNumber(creditCard.getNumber());
+        operation.setAmount(amount);
+        operation.setOperationType(CreditCardOperationType.CHARGE);
+        return operation;
     }
 }
